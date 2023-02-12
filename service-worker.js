60028525cc4a5711847a4f5934538f2c2d753957
jiaqi.zhang <zhangjiaqi9527@gmail.com>
2023-02-12 14:37:10 +0800

    feat(*): 更换主题配置

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 ---
-layout: null
+    layout: null
 ---
-let version = "{{site.time | date: "%Y%m%d%H%M%S"}}"
+    let version = "{{site.time | date: "%Y%m%d%H%M%S"}}"
 let baseUrl = '{{site.baseurl}}'
 
 // let version = '001'
@@ -15,51 +15,51 @@ let cacheKey = cachePrefix + version
 self.skipWaiting()
 
 self.addEventListener('install', function (event) {
-  console.log('serviceWorker install')
-  event.waitUntil(
-    fetch(baseUrl + '/index.html')
-      .then(resp => resp.text())
-      .then(html => {
-        let reg = /(href|src)=\"(.+?)\"/g // 匹配链接
-        let reg2 = /#.*/ // 去除hash
-        let urls = []
-        let match
-        while ((match = reg.exec(html)) !== null) {
-          urls.push(match[2])
-        }
-        urls = urls.map(url => url.replace(reg2, ''))
-        urls = urls.filter(url => !url.startsWith('http') && url.startsWith(baseUrl))
-        urls = Array.from(new Set(urls))
+    console.log('serviceWorker install')
+    event.waitUntil(
+        fetch(baseUrl + '/index.html')
+            .then(resp => resp.text())
+            .then(html => {
+                let reg = /(href|src)=\"(.+?)\"/g // 匹配链接
+                let reg2 = /#.*/ // 去除hash
+                let urls = []
+                let match
+                while ((match = reg.exec(html)) !== null) {
+                    urls.push(match[2])
+                }
+                urls = urls.map(url => url.replace(reg2, ''))
+                urls = urls.filter(url => !url.startsWith('http') && url.startsWith(baseUrl))
+                urls = Array.from(new Set(urls))
 
-        caches.open(cacheKey).then(function (cache) {
-          cache.addAll(urls)
-        })
-      })
-  )
+                caches.open(cacheKey).then(function (cache) {
+                    cache.addAll(urls)
+                })
+            })
+    )
 })
 
 // fetch根据的是cacheKey进行分组，无需等待event.waitUntil
 self.addEventListener('activate', function (event) {
-  console.log('serviceWorker activate')
+    console.log('serviceWorker activate')
 
-  caches.keys().then(keys => {
-    keys.forEach(key => {
-      if (key.startsWith(cachePrefix) && key != cacheKey) caches.delete(key)
+    caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key.startsWith(cachePrefix) && key != cacheKey) caches.delete(key)
+        })
     })
-  })
 })
 
 this.addEventListener('fetch', function (event) {
-  if (new URL(event.request.url).origin != self.origin) {
-    return
-  }
-  event.respondWith(
-    caches
-      .open(cacheKey)
-      .then(cache => cache.match(event.request))
-      .then(data => {
-        if (data) return data
-        return fetch(event.request)
-      })
-  )
+    if (new URL(event.request.url).origin != self.origin) {
+        return
+    }
+    event.respondWith(
+        caches
+            .open(cacheKey)
+            .then(cache => cache.match(event.request))
+            .then(data => {
+                if (data) return data
+                return fetch(event.request)
+            })
+    )
 })
